34595f267abecb243315f9d1e94693fab25d14c4
youngjae459 <youngjae459@gmail.com>
2023-05-17 20:23:06 +0900

    feat: NotProd로 회원 2명을 생성하고 요청 본문(body)에 포함된 내용을 잘 가져오는지 확인

--- src/main/java/com/ll/rest_api/RestApiApplication.java
+++ src/main/java/com/ll/rest_api/RestApiApplication.java
@@ -2,8 +2,10 @@ package com.ll.rest_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // jpa 함수 사용
 public class RestApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ll/rest_api/boundedContext/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.ll.rest_api.boundedContext.member.repository;
+
+import com.ll.rest_api.boundedContext.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+
+}
